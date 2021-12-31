b54f7afc1e226709b4e435d95d0c33b5f90d4035
yoonho <yoonho5684@gmail.com>
2021-12-31 14:05:59 +0900

    refactor : coding convention

--- src/main/kotlin/com/example/jpashopkotlin/domain/Delivery.kt
+++ src/main/kotlin/com/example/jpashopkotlin/domain/Delivery.kt
@@ -3,16 +3,15 @@ package com.example.jpashopkotlin.domain
 import javax.persistence.*
 
 @Entity
-class Delivery(
-    @Id @GeneratedValue @Column(name = "delivery_id")
+data class Delivery(
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
     var id: Long? = null,
-
     @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     var order: Order? = null,
-
     @Embedded
     var address: Address? = null,
-
     @Enumerated(EnumType.ORDINAL)
     var status: DeliveryStatus = DeliveryStatus.READY,
 )
